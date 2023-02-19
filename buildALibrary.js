b2df8d37bb39862051e207db2bd338ca78f45d49
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-19 18:41:34 +0100

    classBook

--- buildALibrary.js
+++ buildALibrary.js
@@ -36,3 +36,17 @@ class Media {
     this.rating.push(value);
   }
 }
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  };
+    get author() {
+      return this._autor;
+    };
+    get pages() {
+      return this._pages;
+    };
+}
